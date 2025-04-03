aac39b4e27c49cf96d6ceb248ce6698afa070da7
Shre <shre@example.com>
2025-04-03 15:20:21 +0530

    The addition file is added successfully

---
+++ addno.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public static void main(String[] arg)
+{
+	Scanner sc = new Scanner(System.in);
+	system.out.println("Enter the two numbers: ");
+	n1 = sc.nextInt();
+	n2 = sc.nextInt();
+
+	int addition;
+	addition = n1 + n2;
+	system.out.println("The addition is: " + addition);
+
+	sc.close();
+}
